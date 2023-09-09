7f9d1dd06c04e6f83ad3344b91801f003db3f764
FreeHK-Lunity <s2020008@lkcss.edu.hk>
2023-09-09 19:59:40 +0800

    more gay stuff

--- main.py
+++ main.py
@@ -17,13 +17,14 @@ def main(e_book_path):
     book = epub.read_epub(f'{e_book_path}')
     e = book.get_metadata('DC', 'title')
     title = e[0][0].strip()
-    print(title)
+    #print(title)
     url = "https://api.myanimelist.net/v2/manga"
     headers = {
         "X-MAL-CLIENT-ID": "239584adfd617ad9c8f7e817be12af25"
     }
     params = {
         "q": f"{title}",
+        "fields": "alternative_titles"
     }
 
     response = requests.get(url, headers=headers, params=params)
@@ -33,7 +34,41 @@ def main(e_book_path):
         json_data = data
         #save to json file
         with open('data.json', 'w',encoding='utf-8') as outfile:
-            json.dump(json_data, outfile)
+            json.dump(json_data, outfile, indent=4)
+        #load json file
+        with open('data.json',encoding='utf-8') as json_file:
+            json_file = json.load(json_file)
+        for item in json_file['data']:
+            if title == item['node']['title'] or item['node']['alternative_titles']['synonyms'] == title or item['node']['alternative_titles']['en'] == title or item['node']['alternative_titles']['ja'] == title:
+                title_exists = True
+                print(f'{title} exists in MAL!!')
+                get_the_id = item['node']['id']
+                break
+            else:
+                title_exists = False
+                print(f'you`re out of luck m8, {title} does not exist in MAL :(')
+                break
+        if title_exists == True:
+            url = f"https://api.myanimelist.net/v2/manga/{get_the_id}"
+            headers = {
+                "X-MAL-CLIENT-ID": "239584adfd617ad9c8f7e817be12af25"
+            }
+            params = {
+                "fields": f"main_picture,alternative_titles,main_picture,authors"
+            }
+            response = requests.get(url, headers=headers, params=params)
+            if response.status_code == 200:
+                data = response.json()
+                # Process the data as needed
+            json_data = data
+            #save to json file
+            with open('data2.json', 'w',encoding='utf-8') as outfile:
+                json.dump(json_data, outfile, indent=4)
+            #load json file
+            with open('data2.json',encoding='utf-8') as json_file:
+                json_file = json.load(json_file)
+            book.set_title(f'{title}')
+                
     else:
         print("Error:", response.status_code)
 main(filename)
