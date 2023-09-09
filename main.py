2dc716c227d64e2c30cfe1dbed1b1a2068b380d8
FreeHK-Lunity <s2020008@lkcss.edu.hk>
2023-09-09 19:09:39 +0800

    add req and add fix some gay things

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-import ebooklib
 from ebooklib import epub
 from tkinter import Tk     # from tkinter import Tk for Python 3.x
 from tkinter.filedialog import askopenfilename
 import requests
-f = open('.env', 'r' , encoding='utf-8')
-client_id = f.read()
-client_id = client_id.replace('client_id = ', '')
-client_id = client_id.replace("'", '')
-client_id = client_id.strip()
+import json
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+client_id = os.getenv("CLIENT_ID")
 print(client_id)
 Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
 filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
@@ -15,10 +16,24 @@ filename = askopenfilename() # show an "Open" dialog box and return the path to 
 def main(e_book_path):
     book = epub.read_epub(f'{e_book_path}')
     e = book.get_metadata('DC', 'title')
-    e = str(e)
-    e = e.replace('[(\'', '')
-    e = e.replace('\',)]', '')
-    e = e.strip()
-    req_mal = requests.get(f'https://api.myanimelist.net/v2/manga?q={e}', f'X-MAL-CLIENT-ID={client_id}' )
-    print(req_mal.status_code)
+    title = e[0][0].strip()
+    print(title)
+    url = "https://api.myanimelist.net/v2/manga"
+    headers = {
+        "X-MAL-CLIENT-ID": "239584adfd617ad9c8f7e817be12af25"
+    }
+    params = {
+        "q": f"{title}",
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code == 200:
+        data = response.json()
+        # Process the data as needed
+        json_data = data
+        #save to json file
+        with open('data.json', 'w',encoding='utf-8') as outfile:
+            json.dump(json_data, outfile)
+    else:
+        print("Error:", response.status_code)
 main(filename)
